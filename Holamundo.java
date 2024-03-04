d97176ddaad974f5ad3458733e2bde50d9135d00
daniel <daniel.carreira@iessanmamede.com>
2024-03-04 10:46:05 +0100

    SE AGREGO LA CLASE Mensaje

--- Holamundo.java
+++ Holamundo.java
@@ -1,8 +1,14 @@
 package Com_acme;
 
 public class Holamundo {
+
+
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        Mensaje msg=new Mensaje();
+        String nombre=msg.solictaDatos("escribe tu nombre: ");
+        msg.saludar(nombre);
+
     }
 
 }
---
+++ Mensaje.java
@@ -0,0 +1,17 @@
+package Com_acme;
+
+import java.util.Scanner;
+
+public class Mensaje {
+
+    public String solictaDatos(String mensaje){
+        System.out.println(mensaje);
+        Scanner scn=new Scanner(System.in);
+        return scn.nextLine();
+    }
+
+    public void saludar(String nombre){
+        System.out.println("hola benvido a java",nombre);
+    }
+
+}
